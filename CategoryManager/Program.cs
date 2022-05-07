3e787e67227661c06ae28b3521708c4a9bd58d4d
Patryk.Dyminski <patryk-dym@o2.pl>
2022-05-07 13:46:45 +0200

    constants.cs

--- CategoryManager/Program.cs
+++ CategoryManager/Program.cs
@@ -3,7 +3,6 @@ using CategoryManager.Category.Factory;
 using CategoryManager.CategoryDeterminer;
 using CategoryManager.Distance;
 using CategoryManager.Manager;
-using CategoryManager.Model;
 using CategoryManager.Relations.Determiner;
 using CategoryManager.Relations.Features;
 using CategoryManager.Relations.Validator;
@@ -11,40 +10,6 @@ using CategoryManager.Repository;
 using CategoryManager.Repository.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-Observation[] observations = new Observation[]
-{
-  //related
-  //1
-  new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,0,0 } },
-  
-  //3
-  new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,1,0 } },
-  
-  //4
-  new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,1,1 } },
-  new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,1,1 } },
-  
-  //6
-  new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,1,0,1 } },
-  new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,1,0,1 } },
-  
-  //not related
-  //6
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {0,1,0,1 } },
-  
-  //10
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,0,0,1 } },
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,0,0,1 } },
-  
-  //13
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,0 } },
-  
-  //14
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,1 } },
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,1 } },
-  new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,1 } },
- };
 
 static IHostBuilder CreateHostBuilder(string[] args)
 {
---
+++ CategoryManager/Utils/Constants.cs
@@ -0,0 +1,40 @@
+﻿using CategoryManager.Model;
+
+namespace CategoryManager.Utils;
+
+internal static class Constants
+{
+  public static Observation[] observations = new Observation[]
+  {
+    //related
+    //1
+    new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,0,0 } },
+    
+    //3
+    new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,1,0 } },
+    
+    //4
+    new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,1,1 } },
+    new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,0,1,1 } },
+    
+    //6
+    new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,1,0,1 } },
+    new Observation{ CategoryId = 1, IsRelated = true, ObservedObject = new int[] {0,1,0,1 } },
+    
+    //not related
+    //6
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {0,1,0,1 } },
+    
+    //10
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,0,0,1 } },
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,0,0,1 } },
+    
+    //13
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,0 } },
+    
+    //14
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,1 } },
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,1 } },
+    new Observation{ CategoryId = 1, IsRelated = false, ObservedObject = new int[] {1,1,0,1 } },
+  };
+}
